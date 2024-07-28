4a146759a0d2c326aa7aacd3dbb4f8ed8a95add4
Haezeldavids <haezelmogot026@student.unsrat.ac.id>
2024-07-29 01:42:35 +0800

    FIX FIELD

--- admin/pages/standar/standar_nasional.php
+++ admin/pages/standar/standar_nasional.php
@@ -1,27 +1,6 @@
 <?php 
     require '../koneksi/koneksi.php';
-// mencari kode barang dengan nilai paling besar
-// $query = "SELECT max(kode_kamera) as maxKode FROM kamera";
-// $hasil = mysqli_query($connect,$query);
-// $data = mysqli_fetch_array($hasil);
-// $kodeKamera = $data['maxKode'];
-// 
-// mengambil angka atau bilangan dalam kode anggota terbesar,
-// dengan cara mengambil substring mulai dari karakter ke-1 diambil 6 karakter
-// misal 'BRG001', akan diambil '001'
-// setelah substring bilangan diambil lantas dicasting menjadi integer
-// $noUrut = (int) substr($kodeKamera, 3, 3);
 
-// bilangan yang diambil ini ditambah 1 untuk menentukan nomor urut berikutnya
-// $noUrut++;
-
-// membentuk kode anggota baru
-// perintah sprintf("%03s", $noUrut); digunakan untuk memformat string sebanyak 3 karakter
-// misal sprintf("%03s", 12); maka akan dihasilkan '012'
-// atau misal sprintf("%03s", 1); maka akan dihasilkan string '001'
-// $char = "KMR";
-// $kodeBarang = $char . sprintf("%03s", $noUrut);
-// echo $kodeBarang;
 
 
 ?>
@@ -82,8 +61,8 @@
                 <thead>
                 <tr>
                   <th>No</th>
-                  <th>Kode Standar</th>
-                  <th>Nama Standar</th>
+                  <th>Kode Standar Nasional</th>
+                  <th>Nama Standar Nasional</th>
                   <th>Tanggal Standar</th>
                   
                   <th><center>Pengaturan Data</center></th>
@@ -134,17 +113,7 @@
                   </tr>
                    <?php } ?>
                 </tbody>
-                <!-- <tfoot>
-                <tr>
-                  <th>No</th>
-                  <th>Kode Standar</th>
-                  <th>Nama Standar</th>
-                  <th>Tanggal Standar</th>
-              
                 
-                
-                </tr>
-                </tfoot> -->
               </table>
             </div>
           </div>
@@ -170,11 +139,11 @@
                          <div class="form-group">
                           
                               <label for="kode_sn">Kode Standar Nasional</label>
-                              <input type="text" class="form-control" name="kode_sn" id="kode_sn" placeholder="Masukan Kode Standar Nasional">   
+                              <input type="text" required class="form-control" name="kode_sn" id="kode_sn" placeholder="Masukan Kode Standar Nasional">   
                           </div>
                           <div class="form-group">
                             <label for="nama_sn">Nama Standar Nasional</label>
-                            <input type="text" class="form-control" name="nama_sn" id="nama_sn" placeholder="Masukan Nama Standar Nasional">
+                            <input type="text" required class="form-control" name="nama_sn" id="nama_sn" placeholder="Masukan Nama Standar Nasional">
                           </div>
 
                           
